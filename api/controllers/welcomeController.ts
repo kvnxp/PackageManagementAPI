16bed5245b31ff69742ea63f09c1694a4be53b7d
Kevin Molinares <kvnxp@hotmail.com>
2025-02-07 17:12:01 -0500

    Reactor code that no work ..

--- api/controllers/welcomeController.ts
+++ api/controllers/welcomeController.ts
@@ -1,4 +1,3 @@
 export default function welcome(req:any, res:any) {
-
     res.send("Welcome to the API"); 
 }
--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,11 @@
 import "dotenv/config"
 import { envInit } from "./server/envInit";
 import { expressInit } from "./server/express";
-import * as Sentry from "@sentry/node"
-
-export const sentry = Sentry.init({
-  dsn: process.env.SENTRYDSN,
-  tracesSampleRate: 1.0,
-})
+import { sentryStarup } from "./server/sentryReport";
 
 envInit();
 const app = expressInit();
-Sentry.setupExpressErrorHandler(app);
+sentryStarup();
 
 // this output for Vercel serverless function
 export default app;
