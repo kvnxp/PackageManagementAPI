8cb533440caa2f57405160fde78748605af7217a
Kevin Molinares <kvnxp@hotmail.com>
2025-02-07 17:10:37 -0500

    [FT] Error validation

---
+++ api/server/sentryReport.ts
@@ -0,0 +1,22 @@
+import * as Sentry from '@sentry/node';
+
+let SENTRYAPP: any;
+export function sentryStarup() {
+    const sentry = Sentry.init({
+        dsn: process.env.SENTRYDSN,
+        tracesSampleRate: 1.0,
+    })
+    SENTRYAPP = sentry;
+    return sentry;
+}
+
+export function printError(err: Error) {
+    SENTRYAPP.captureException(err);
+    console.log(err.message);
+    console.log(err.stack);
+}
+
+export function printLog(message: string) {
+    SENTRYAPP.captureMessage(message);
+    console.log(message);
+}
