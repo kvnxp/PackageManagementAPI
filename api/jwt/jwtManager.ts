079deb437961dd87dc441fda3e730a59057f8937
Kevin Molinares <kvnxp@hotmail.com>
2025-02-09 16:43:36 -0500

    [ADD] jwt sign and verify

--- api/jwt/jwtManager.ts
+++ api/jwt/jwtManager.ts
@@ -1,10 +1,33 @@
-import * as JWT from "jose";
+import * as jwt from "jose"
+import { enviromentKeys } from "../server/enviromentKeys";
+import { responseStruct } from "../struct/responseStruct";
+
 export class JWTManager {
+    public static async createToken(data: any) {
+        try {
 
-    public static async sign(payload: any, key: JWT.KeyLike, options: JWT.SignOptions) {
+            const secret = new TextEncoder().encode(enviromentKeys.JWT_SECRET);
+            const alg = "HS256";
+            const expiresIn = "12h";
+            const token = await new jwt.SignJWT(data).setProtectedHeader({ alg: alg }).setIssuedAt().setExpirationTime(expiresIn).sign(secret);
+            return token;
 
+        } catch (error: any) {
+            throw new responseStruct("error", error.message, 500);
+        }
 
     }
 
+    public static async verifyToken(token: string) {
+        try {
+
+            const secret = new TextEncoder().encode(enviromentKeys.JWT_SECRET);
+            const payload = await jwt.jwtVerify(token, secret, { algorithms: ["HS256"] });
+            return payload;
+
+        } catch (error: any) {
+            throw new responseStruct("error", error.message, 500);
+        }
+    }
 
 }
---
+++ api/tools/messageList.ts
@@ -0,0 +1,19 @@
+export class MessageList {
+    public static readonly USER_NOT_FOUND = "User not found";
+    public static readonly USER_EXISTS = "User exists";
+    public static readonly USER_CREATED = "User created";
+    public static readonly USER_UPDATED = "User updated";
+
+    public static readonly DRIVER_CREATED = "Driver created";
+    public static readonly DRIVER_UPDATED = "Driver updated";
+
+    public static readonly VEHICLE_CREATED = "Vehicle created";
+    public static readonly VEHICLE_UPDATED = "Vehicle updated";
+
+    public static readonly AUTH_INVALID_EMAIL = "Invalid email format";
+    public static readonly AUTH_INVALID_DATE = "Invalid date format";
+    public static readonly AUTH_INVALID_PASSWORD = "The password is incorrect";
+    public static readonly AUTH_INVALID_CREDENTIALS = "Invalid credentials";
+    public static readonly AUTH_SUCCESS = "Authentication successful";
+
+}
