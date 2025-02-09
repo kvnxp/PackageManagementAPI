ba5b15e8db8cbe8e5b303101429806c3d0aea61e
Kevin Molinares <kvnxp@hotmail.com>
2025-02-09 01:38:18 -0500

    [FIX] changes on structure of table

--- sqlTemplate.sql
+++ sqlTemplate.sql
@@ -1,25 +1,24 @@
 -- Date: 2021-06-06 22:00:00
 CREATE TABLE `users` (
-  `id` uuid NOT NULL DEFAULT uuid(),
-  `idCard` varchar(20) NOT NULL,
+  `id` varchar(36) NOT NULL ,
+  `idCard` INT(20) NOT NULL,
   `firstName` varchar(30) NOT NULL,
   `lastName` varchar(30) NOT NULL,
   `gender` int(2) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `dateBirth` int(11) NOT NULL,
-  `createAt` timestamp NOT NULL,
-  `updateAt` datetime NOT NULL,
+  `postalCode` INT(10) ,
+  `email` varchar(100) NOT NULL,
+  `phone` INT(55) NOT NULL,
+  `dateBirth` DATE,
+  `createdAt` timestamp NOT NULL,
+  `updatedAt` datetime NOT NULL,
   `role` int(11) NOT NULL,
-  `passwd` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB ;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `idCard` (`idCard`);
