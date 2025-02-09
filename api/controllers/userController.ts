1855d5de4dacb7ea7a34a4cc4c99061d926ba1fd
Kevin Molinares <kvnxp@hotmail.com>
2025-02-09 16:38:15 -0500

    refact user controller and enum, remove user Interface for use User class

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -1,46 +1,12 @@
 import { Request, Response } from "express";
-import { SQL } from "../server/mysqlManager";
-import { UserQueryList } from "../sql/userQuery";
-import { User } from "../struct/userStruct";
-import * as  bcrypt from 'bcrypt';
-import { Roles } from "../enums/roleEnum";
-import { responseStruct } from "../struct/responseStruct";
 
 export class UserController {
-
-    public static async login(req: Request, res: Response) {
-        res.send("User Login");
+    static getUser(req: Request, res: Response, next: any) {
+        res.send("Hello World");
     }
 
-    public static async register(req: Request, res: Response, next: any) {
-        const body: User = req.body;
+    public static async getUserList(req: Request, res: Response, next: any) {
 
-        let userList: any[];
 
-        try {
-            userList = await SQL.promise().query(UserQueryList.SELECT_USER_BY_IDCARD, [body.idCard]);
-
-        } catch (error) {
-            return;
-        }
-
-        try {
-            const userData: User = new User(body);
-            userData.password = bcrypt.hashSync(userData.password, 10);
-            userData.role = Roles.user;
-            const values = User.toInsert(userData);
-
-            const query = `INSERT INTO users (${values.keyes}) VALUES (${values.comodin})`;
-
-            const result = await SQL.promise().query(query, values.values);
-
-            res.send(new responseStruct("ok", "User created"));
-
-            console.log("write user to database");
-
-        }
-        catch (error) {
-            next(error);
-        }
     }
 }
--- api/enums/roleEnum.ts
+++ api/enums/roleEnum.ts
@@ -1,6 +1,6 @@
-export enum Roles{
-    root=0,
-    admin=1,
-    user=2,
-    transport=3
+export enum Roles {
+    guest = 0,
+    admin = 1,
+    user = 2,
+    transport = 3
 }
